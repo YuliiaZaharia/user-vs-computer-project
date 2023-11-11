bdb8ed5066f2509615372d508a1e045fce4a7b2d
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-11 17:42:22 +0300

    used methods querySelector and addEvenListener

---
+++ index.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("#guess");
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", play);
+
+function play() {
+    const userNumber = document.querySelector("#guess").value;
+}
