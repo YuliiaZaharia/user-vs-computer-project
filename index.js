cd59ce2456237efd82c526c8d54baebd6dbfd22c
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-11 17:53:02 +0300

    added alert in JS

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ button.addEventListener("click", play);
 
 function play() {
     const userNumber = document.querySelector("#guess").value;
+
+    if (userNumber < 1 || userNumber > 20) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Enter number from 1 to 20!",
+          })
+    }
 }
