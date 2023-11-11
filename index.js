9944abc85933e2101216be006f7b55ff00ad5373
YuliiaZaharia <juliarybalchenko999@gmail.com>
2023-11-11 18:37:33 +0300

    last touches in JS

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const input = document.querySelector("#guess");
 const button = document.querySelector(".btn");
 const answer = Math.floor(Math.random()*20)+1;
 
+input.addEventListener("keypress", function(e) {
+    if (e.keyCode === 13) {
+        play();
+    }
+})
+
 button.addEventListener("click", play);
 
 function play() {
@@ -24,4 +30,26 @@ function play() {
     }
 
 
+    else {
+        if (userNumber < answer) {
+            Swal.fire('Computer is winning for now!', 
+            'Enter number higher!')
+        }
+
+        else if (userNumber > answer) {
+            Swal.fire('Computer is winning for now!',
+            'Enter number lower!')
+        }
+
+        else {
+            Swal.fire({
+                title: 'Victory!',
+                text: "Hugs and congratulations!",
+                imageUrl: "https://images.unsplash.com/photo-1530103862676-de8c9debad1d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                imageWidth: 400,
+                imageHeight: 200,
+                imageAlt: 'Victory'
+              })
+        }
+    }
 }
